a93222bdae2585a1519de08380764eccd822497e
hanschristian2710 <Hanschristian@Hanss-MacBook-Pro-3.local>
2017-04-17 22:24:03 -0700

    qunit testing -ci-skip

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+QUnit.test( "Testing starter.js", function(assert) {
+	
+	// Test 
+	var test1 = "baz", foo1 = foo(test1); // Will gives true
+	var test2 = "lol", foo2 = foo(test2); // False
+	var test3 = "bar", foo3 = foo(test3); // False
+
+	// Assert testings
+  	assert.equal(foo1, true, "foo1 == baz");
+ 	assert.notEqual(foo2, true,"foo2 != baz!");
+  	assert.notEqual(foo3, true,"foo3 != baz!");
+
+});
