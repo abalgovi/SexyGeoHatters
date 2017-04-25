aa2052865a097212434ef753f116a305ff72f9f3
abalgovi <abalgovi@ucsd.edu>
2017-04-24 17:01:56 -0700

    finished passCheck --ci-skip

--- public/javascripts/checkPswd.js
+++ public/javascripts/checkPswd.js
@@ -1,8 +1,12 @@
 
 function checkPswd() {
-  
+ 
+  // replace this regex with the one you retrieve from the db
   var regex = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!?*@#$%&_])[A-Za-z0-9!@#$%&*?_]{8,20}/;
-  var userInput = document.getElementById('userinput'), badPass = document.getElementById('passFailed');
+  var userInput = document.getElementById('password'), badPass = document.getElementById('passFailed');
+  
+  // persist the data
+  usrObj.insertUser({username: 'admin', password: 'sexygeohatters'});
 
   if(!regex.test(userInput.value)) {
     badPass.innerHTML = 'Password must be 8 - 20 characters long and have' + 
