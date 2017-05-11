2971b20aaf1c05ba0760e328616898a4691819f9
hanschristian2710 <Hanschristian@Hanss-MacBook-Pro-3.local>
2017-05-11 16:36:14 -0700

    merge with team2 --ci-skip

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.io = require('socket.io')();
 
 global.__base = __dirname + '/';
 
-var mongoURI = process.env.MONGOLAB_URI || 'YOUR MONGODB CONNECTION ON mLAB';
+var mongoURI = process.env.MONGOLAB_URI || 'mongodb://randomgeohatter:sexygeohatters@ds115071.mlab.com:15071/sexygeohatters';
 //var mongoURI = process.env.MONGOLAB_URI || 'mongodb://admin:admin@ds061335.mongolab.com:61335/heroku_w9bxpzpc';
 console.log('Connecting to DB: ' + mongoURI);
 var db = monk(mongoURI);
