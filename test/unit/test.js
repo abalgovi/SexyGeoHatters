5a5e0eb3e8512a17d9481b2e49fd8724bcd5b6c9
hanschristian2710 <Hanschristian@Hanss-MacBook-Pro-3.local>
2017-05-15 12:20:28 -0700

    Cleaning stuffs --ci-skip

---
+++ test/unit/test.js
@@ -0,0 +1,8 @@
+var chai = require('chai');  // Required to test node.js locally
+var expect = chai.expect;    
+
+describe ("Test", function(){
+	it("should work", function () {
+		expect(true).to.be.true;
+	});
+});
