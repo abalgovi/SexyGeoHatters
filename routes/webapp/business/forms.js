8e2dbf7bb973de019c57edd7068c1e86b7dda75f
abalgovi <abalgovi@ucsd.edu>
2017-05-23 17:48:57 -0700

    saved forms to mongo --ci-skip

--- routes/webapp/business/forms.js
+++ routes/webapp/business/forms.js
@@ -1,7 +1,6 @@
 var crypto = require('crypto');
 var baby = require('babyparse');
 var async = require('async');
-// var sendgrid  = require('sendgrid')('robobetty', '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');
 var sendgrid = require('sendgrid')('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');
 var ObjectId = require('mongodb').ObjectID;
 
@@ -27,11 +26,46 @@ exports.get = function(req,res,next){
     var inputEmail = req.body.inputEmail;
     var inputPhone = req.body.inputPhone;
 
-
-        res.render('business/forms', {
+    var responseParams =  {
         title: 'Form Editor',
         isOwner: req.user[0].admin,
         businessId: req.user[0].business,
-        employees: "active"
+        employees: "active" 
+    }
+
+    var formEdit = req.query.formEdit;
+    if(formEdit) {
+    	if(formEdit == 'success') responseParams.success = true;
+    	else if(formEdit == 'error') responseParams.error = true;
+	else responseParams.empty = true;
+    }
+
+    res.render('business/forms', responseParams);
+}
+
+exports.post = function(req,res,next) {
+
+    var formsCollection = req.db.get('forms');
+    var businessID = req.user[0].business;
+    var formJson = req.body.formData;
+
+    if(formJson == null || formJson.length <= 2) {
+        res.redirect('/forms?formEdit=empty');
+	return;
+    }
+
+    formsCollection.findOneAndUpdate({businessId: businessID},
+		    {businessId: businessID, formFields: formJson}).then((doc) => {
+	
+	// if document is not in collection insert a new one
+	if(!doc.value && doc.lastErrorObject)
+	    formsCollection.insert({businessId: businessID, formParams: formJson}).then((newDoc) => {});
+	return res.redirect('/forms?formEdit=success');
+
+    }).catch((err) => {
+	console.log('Error when updating form structure ' + err);
+	res.redirect('/forms?formEdit=error');
     });
+    
+
 }
