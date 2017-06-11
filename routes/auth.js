fcea9e380be00f5b4f42e6381b4691c0762b9024
TaylorTakao <taylortakao@gmail.com>
2017-06-10 21:22:26 -0700

    adding webhooks

--- routes/auth.js
+++ routes/auth.js
@@ -41,9 +41,6 @@ router.post('/employeeregister',passport.authenticate('local-signup-employee',{
 }));
 
 
-
-
-
 router.get('/profile', isLoggedIn, function(req, res) {
     res.render('auth/profile.hjs', {
         user : req.user // get the user out of session and pass to template
